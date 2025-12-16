49f4b136fe0d070ead3c6eff8f1b4d428bcaf028
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2025-12-16 16:59:33 +0200

    Add putObject

--- src/main.zig
+++ src/main.zig
@@ -6,8 +6,7 @@ pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
 
-
-    var server = try httpz.Server(void).init(allocator, .{.port=3000}, {});
+    var server = try httpz.Server(void).init(allocator, .{ .port = 3000 }, {});
     defer {
         server.stop();
         server.deinit();
@@ -17,12 +16,12 @@ pub fn main() !void {
     router.get("/api/user/:id", get_user, .{});
     router.get("/health", x3_zig.health_endpoind_handler, .{});
     router.put("/:bucket_name", x3_zig.put_bucket, .{});
+    router.put("/:bucket_name/:object_name", x3_zig.put_object, .{});
 
     try server.listen();
-
 }
 
 fn get_user(req: *httpz.Request, res: *httpz.Response) !void {
     res.status = 200;
-    try res.json(.{.id = req.param("id").?, .name = "Bob"}, .{});
+    try res.json(.{ .id = req.param("id").?, .name = "Bob" }, .{});
 }
